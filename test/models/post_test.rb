9904974e40c787aa3347b0a71a968ecf259928ed
Fuya <dev@fuya.info>
2014-09-29 04:37:11 +0900

    テスト書きました MarkdwonのH1が展開出来てるか
    それ以外は http://guides.rubyonrails.org/testing.html 参考

--- test/models/post_test.rb
+++ test/models/post_test.rb
@@ -4,4 +4,14 @@ class PostTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  fixtures :posts
+
+  def test_simple_fixture
+    post = posts(:simple)
+    assert_equal "Title01", post.title
+    assert_equal "Hello world!", post.body
+  end
+
+
+
 end
