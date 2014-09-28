bf915c8af22a6338ab0257e794fd4791da0d7d2c
Fuya <dev@fuya.info>
2014-09-29 02:38:30 +0900

    add comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,9 @@
 class CommentsController < ApplicationController
+
+  def create
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.create(params[:comment].permit(:commenter, :body))
+    redirect_to posts_path(@post)
+  end
+
 end
