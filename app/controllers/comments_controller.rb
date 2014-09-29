0f0fbd442e5a35edae31450ef80d710ac18a637f
Fuya <dev@fuya.info>
2014-09-29 19:44:46 +0900

    basic auth

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,7 @@
 class CommentsController < ApplicationController
 
+  http_basic_authenticate_with name: Settings.admin.user, password: Settings.admin.password, only: :destroy
+
   def create
     @post = Post.find(params[:post_id])
     @comment = @post.comments.create(params[:comment].permit(:commenter, :body))
