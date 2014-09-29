b67cd09f1505b0f28d49725985494d9cda82108b
Fuya <dev@fuya.info>
2014-09-29 23:53:43 +0900

    トップページ変えてなかった

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
   end
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'posts#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
