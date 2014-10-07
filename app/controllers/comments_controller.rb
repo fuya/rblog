3d9d1979af9b52c249e6e4700552663d783a1bd5
Fuya <dev@fuya.info>
2014-10-07 11:40:37 +0900

    コメント投稿後に投稿ページに戻るように修正

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -5,7 +5,7 @@ class CommentsController < ApplicationController
   def create
     @post = Post.find(params[:post_id])
     @comment = @post.comments.create(params[:comment].permit(:commenter, :body))
-    redirect_to posts_path(@post)
+    redirect_to post_path(@post)
   end
 
 
